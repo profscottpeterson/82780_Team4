46510bf9f4df2d237d80098a6d05b79215a02e3c
Renell Sherman <renell.sherman@mymail.nwtc.edu>
2018-10-09 14:11:27 -0500

    Basic UI Test
    Created Basic UI for the project. Want to get oppinion from other teammates before completting final forms.

---
+++ TeamDevProject/CustomersDelete.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace TeamDevProject
+{
+    partial class CustomersDelete
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExitCustomerDelete = new System.Windows.Forms.Button();
+            this.btnReturnCustomerDelete = new System.Windows.Forms.Button();
+            this.btnResetCustomerDelete = new System.Windows.Forms.Button();
+            this.btnCustomerDeleteGo = new System.Windows.Forms.Button();
+            this.cbxAddressNullCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.cbxLNameNullCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.cbxFNameNullCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.txtAddressCustomerDelete = new System.Windows.Forms.TextBox();
+            this.txtLNameCustomerDelete = new System.Windows.Forms.TextBox();
+            this.txtFNameCustomerDelete = new System.Windows.Forms.TextBox();
+            this.cbxAddressOnCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.cbxLNameOnCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.cbxFNameOnCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.lblCustomerDeleteTitle = new System.Windows.Forms.Label();
+            this.txtIDCustomerDelete = new System.Windows.Forms.TextBox();
+            this.cbxIDOnCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.cbxIDNullCustomerDelete = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnExitCustomerDelete
+            // 
+            this.btnExitCustomerDelete.Location = new System.Drawing.Point(416, 173);
+            this.btnExitCustomerDelete.Name = "btnExitCustomerDelete";
+            this.btnExitCustomerDelete.Size = new System.Drawing.Size(154, 39);
+            this.btnExitCustomerDelete.TabIndex = 28;
+            this.btnExitCustomerDelete.Text = "Exit";
+            this.btnExitCustomerDelete.UseVisualStyleBackColor = true;
+            this.btnExitCustomerDelete.Click += new System.EventHandler(this.btnExitCustomerDelete_Click);
+            // 
+            // btnReturnCustomerDelete
+            // 
+            this.btnReturnCustomerDelete.Location = new System.Drawing.Point(416, 128);
+            this.btnReturnCustomerDelete.Name = "btnReturnCustomerDelete";
+            this.btnReturnCustomerDelete.Size = new System.Drawing.Size(154, 39);
+            this.btnReturnCustomerDelete.TabIndex = 29;
+            this.btnReturnCustomerDelete.Text = "Return";
+            this.btnReturnCustomerDelete.UseVisualStyleBackColor = true;
+            this.btnReturnCustomerDelete.Click += new System.EventHandler(this.btnReturnCustomerDelete_Click);
+            // 
+            // btnResetCustomerDelete
+            // 
+            this.btnResetCustomerDelete.Location = new System.Drawing.Point(416, 83);
+            this.btnResetCustomerDelete.Name = "btnResetCustomerDelete";
+            this.btnResetCustomerDelete.Size = new System.Drawing.Size(154, 39);
+            this.btnResetCustomerDelete.TabIndex = 30;
+            this.btnResetCustomerDelete.Text = "Reset";
+            this.btnResetCustomerDelete.UseVisualStyleBackColor = true;
+            this.btnResetCustomerDelete.Click += new System.EventHandler(this.btnResetCustomerDelete_Click);
+            // 
+            // btnCustomerDeleteGo
+            // 
+            this.btnCustomerDeleteGo.Location = new System.Drawing.Point(416, 15);
+            this.btnCustomerDeleteGo.Name = "btnCustomerDeleteGo";
+            this.btnCustomerDeleteGo.Size = new System.Drawing.Size(154, 62);
+            this.btnCustomerDeleteGo.TabIndex = 31;
+            this.btnCustomerDeleteGo.Text = "Delete";
+            this.btnCustomerDeleteGo.UseVisualStyleBackColor = true;
+            // 
+            // cbxAddressNullCustomerDelete
+            // 
+            this.cbxAddressNullCustomerDelete.Enabled = false;
+            this.cbxAddressNullCustomerDelete.Location = new System.Drawing.Point(263, 136);
+            this.cbxAddressNullCustomerDelete.Name = "cbxAddressNullCustomerDelete";
+            this.cbxAddressNullCustomerDelete.Size = new System.Drawing.Size(98, 21);
+            this.cbxAddressNullCustomerDelete.TabIndex = 25;
+            this.cbxAddressNullCustomerDelete.Text = "Null";
+            this.cbxAddressNullCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxAddressNullCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxAddressNullCustomerDelete_CheckedChanged);
+            // 
+            // cbxLNameNullCustomerDelete
+            // 
+            this.cbxLNameNullCustomerDelete.Enabled = false;
+            this.cbxLNameNullCustomerDelete.Location = new System.Drawing.Point(263, 95);
+            this.cbxLNameNullCustomerDelete.Name = "cbxLNameNullCustomerDelete";
+            this.cbxLNameNullCustomerDelete.Size = new System.Drawing.Size(98, 21);
+            this.cbxLNameNullCustomerDelete.TabIndex = 26;
+            this.cbxLNameNullCustomerDelete.Text = "Null";
+            this.cbxLNameNullCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxLNameNullCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxLNameNullCustomerDelete_CheckedChanged);
+            // 
+            // cbxFNameNullCustomerDelete
+            // 
+            this.cbxFNameNullCustomerDelete.Enabled = false;
+            this.cbxFNameNullCustomerDelete.Location = new System.Drawing.Point(263, 56);
+            this.cbxFNameNullCustomerDelete.Name = "cbxFNameNullCustomerDelete";
+            this.cbxFNameNullCustomerDelete.Size = new System.Drawing.Size(98, 21);
+            this.cbxFNameNullCustomerDelete.TabIndex = 27;
+            this.cbxFNameNullCustomerDelete.Text = "Null";
+            this.cbxFNameNullCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxFNameNullCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxFNameNullCustomerDelete_CheckedChanged);
+            // 
+            // txtAddressCustomerDelete
+            // 
+            this.txtAddressCustomerDelete.Enabled = false;
+            this.txtAddressCustomerDelete.Location = new System.Drawing.Point(122, 136);
+            this.txtAddressCustomerDelete.Name = "txtAddressCustomerDelete";
+            this.txtAddressCustomerDelete.Size = new System.Drawing.Size(135, 22);
+            this.txtAddressCustomerDelete.TabIndex = 22;
+            // 
+            // txtLNameCustomerDelete
+            // 
+            this.txtLNameCustomerDelete.Enabled = false;
+            this.txtLNameCustomerDelete.Location = new System.Drawing.Point(122, 95);
+            this.txtLNameCustomerDelete.Name = "txtLNameCustomerDelete";
+            this.txtLNameCustomerDelete.Size = new System.Drawing.Size(135, 22);
+            this.txtLNameCustomerDelete.TabIndex = 23;
+            // 
+            // txtFNameCustomerDelete
+            // 
+            this.txtFNameCustomerDelete.Enabled = false;
+            this.txtFNameCustomerDelete.Location = new System.Drawing.Point(122, 56);
+            this.txtFNameCustomerDelete.Name = "txtFNameCustomerDelete";
+            this.txtFNameCustomerDelete.Size = new System.Drawing.Size(135, 22);
+            this.txtFNameCustomerDelete.TabIndex = 24;
+            // 
+            // cbxAddressOnCustomerDelete
+            // 
+            this.cbxAddressOnCustomerDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxAddressOnCustomerDelete.AutoSize = true;
+            this.cbxAddressOnCustomerDelete.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbxAddressOnCustomerDelete.Location = new System.Drawing.Point(34, 137);
+            this.cbxAddressOnCustomerDelete.Name = "cbxAddressOnCustomerDelete";
+            this.cbxAddressOnCustomerDelete.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cbxAddressOnCustomerDelete.Size = new System.Drawing.Size(82, 21);
+            this.cbxAddressOnCustomerDelete.TabIndex = 20;
+            this.cbxAddressOnCustomerDelete.Text = "Address";
+            this.cbxAddressOnCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxAddressOnCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxAddressOnCustomerDelete_CheckedChanged);
+            // 
+            // cbxLNameOnCustomerDelete
+            // 
+            this.cbxLNameOnCustomerDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxLNameOnCustomerDelete.AutoSize = true;
+            this.cbxLNameOnCustomerDelete.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbxLNameOnCustomerDelete.Location = new System.Drawing.Point(18, 95);
+            this.cbxLNameOnCustomerDelete.Name = "cbxLNameOnCustomerDelete";
+            this.cbxLNameOnCustomerDelete.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cbxLNameOnCustomerDelete.Size = new System.Drawing.Size(98, 21);
+            this.cbxLNameOnCustomerDelete.TabIndex = 21;
+            this.cbxLNameOnCustomerDelete.Text = "Last Name";
+            this.cbxLNameOnCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxLNameOnCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxLNameOnCustomerDelete_CheckedChanged);
+            // 
+            // cbxFNameOnCustomerDelete
+            // 
+            this.cbxFNameOnCustomerDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxFNameOnCustomerDelete.AutoSize = true;
+            this.cbxFNameOnCustomerDelete.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbxFNameOnCustomerDelete.Location = new System.Drawing.Point(18, 57);
+            this.cbxFNameOnCustomerDelete.Name = "cbxFNameOnCustomerDelete";
+            this.cbxFNameOnCustomerDelete.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cbxFNameOnCustomerDelete.Size = new System.Drawing.Size(98, 21);
+            this.cbxFNameOnCustomerDelete.TabIndex = 19;
+            this.cbxFNameOnCustomerDelete.Text = "First Name";
+            this.cbxFNameOnCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxFNameOnCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxFNameOnCustomerDelete_CheckedChanged);
+            // 
+            // lblCustomerDeleteTitle
+            // 
+            this.lblCustomerDeleteTitle.AutoSize = true;
+            this.lblCustomerDeleteTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerDeleteTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblCustomerDeleteTitle.Name = "lblCustomerDeleteTitle";
+            this.lblCustomerDeleteTitle.Size = new System.Drawing.Size(182, 32);
+            this.lblCustomerDeleteTitle.TabIndex = 18;
+            this.lblCustomerDeleteTitle.Text = "Delete Fields";
+            // 
+            // txtIDCustomerDelete
+            // 
+            this.txtIDCustomerDelete.Enabled = false;
+            this.txtIDCustomerDelete.Location = new System.Drawing.Point(122, 173);
+            this.txtIDCustomerDelete.Name = "txtIDCustomerDelete";
+            this.txtIDCustomerDelete.Size = new System.Drawing.Size(135, 22);
+            this.txtIDCustomerDelete.TabIndex = 22;
+            // 
+            // cbxIDOnCustomerDelete
+            // 
+            this.cbxIDOnCustomerDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxIDOnCustomerDelete.AutoSize = true;
+            this.cbxIDOnCustomerDelete.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbxIDOnCustomerDelete.Location = new System.Drawing.Point(73, 173);
+            this.cbxIDOnCustomerDelete.Name = "cbxIDOnCustomerDelete";
+            this.cbxIDOnCustomerDelete.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.cbxIDOnCustomerDelete.Size = new System.Drawing.Size(43, 21);
+            this.cbxIDOnCustomerDelete.TabIndex = 20;
+            this.cbxIDOnCustomerDelete.Text = "ID";
+            this.cbxIDOnCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxIDOnCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxIDOnCustomerDelete_CheckedChanged);
+            // 
+            // cbxIDNullCustomerDelete
+            // 
+            this.cbxIDNullCustomerDelete.Enabled = false;
+            this.cbxIDNullCustomerDelete.Location = new System.Drawing.Point(263, 173);
+            this.cbxIDNullCustomerDelete.Name = "cbxIDNullCustomerDelete";
+            this.cbxIDNullCustomerDelete.Size = new System.Drawing.Size(98, 21);
+            this.cbxIDNullCustomerDelete.TabIndex = 25;
+            this.cbxIDNullCustomerDelete.Text = "Null";
+            this.cbxIDNullCustomerDelete.UseVisualStyleBackColor = true;
+            this.cbxIDNullCustomerDelete.CheckedChanged += new System.EventHandler(this.cbxIDNullCustomerDelete_CheckedChanged);
+            // 
+            // CustomersDelete
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(624, 228);
+            this.Controls.Add(this.btnExitCustomerDelete);
+            this.Controls.Add(this.btnReturnCustomerDelete);
+            this.Controls.Add(this.btnResetCustomerDelete);
+            this.Controls.Add(this.btnCustomerDeleteGo);
+            this.Controls.Add(this.cbxIDNullCustomerDelete);
+            this.Controls.Add(this.cbxAddressNullCustomerDelete);
+            this.Controls.Add(this.cbxLNameNullCustomerDelete);
+            this.Controls.Add(this.cbxFNameNullCustomerDelete);
+            this.Controls.Add(this.txtIDCustomerDelete);
+            this.Controls.Add(this.txtAddressCustomerDelete);
+            this.Controls.Add(this.txtLNameCustomerDelete);
+            this.Controls.Add(this.txtFNameCustomerDelete);
+            this.Controls.Add(this.cbxIDOnCustomerDelete);
+            this.Controls.Add(this.cbxAddressOnCustomerDelete);
+            this.Controls.Add(this.cbxLNameOnCustomerDelete);
+            this.Controls.Add(this.cbxFNameOnCustomerDelete);
+            this.Controls.Add(this.lblCustomerDeleteTitle);
+            this.Name = "CustomersDelete";
+            this.Text = "CustomersDelete";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExitCustomerDelete;
+        private System.Windows.Forms.Button btnReturnCustomerDelete;
+        private System.Windows.Forms.Button btnResetCustomerDelete;
+        private System.Windows.Forms.Button btnCustomerDeleteGo;
+        private System.Windows.Forms.CheckBox cbxAddressNullCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxLNameNullCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxFNameNullCustomerDelete;
+        private System.Windows.Forms.TextBox txtAddressCustomerDelete;
+        private System.Windows.Forms.TextBox txtLNameCustomerDelete;
+        private System.Windows.Forms.TextBox txtFNameCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxAddressOnCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxLNameOnCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxFNameOnCustomerDelete;
+        private System.Windows.Forms.Label lblCustomerDeleteTitle;
+        private System.Windows.Forms.TextBox txtIDCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxIDOnCustomerDelete;
+        private System.Windows.Forms.CheckBox cbxIDNullCustomerDelete;
+    }
+}
---
+++ TeamDevProject/InventoryMain.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TeamDevProject
+{
+    partial class InventoryMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInventoryDelete = new System.Windows.Forms.Button();
+            this.btnInventoryAdd = new System.Windows.Forms.Button();
+            this.btnInventorySearch = new System.Windows.Forms.Button();
+            this.lbxInventoryMenu = new System.Windows.Forms.ListBox();
+            this.lblInventoryTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInventoryDelete
+            // 
+            this.btnInventoryDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventoryDelete.Location = new System.Drawing.Point(24, 267);
+            this.btnInventoryDelete.Name = "btnInventoryDelete";
+            this.btnInventoryDelete.Size = new System.Drawing.Size(114, 44);
+            this.btnInventoryDelete.TabIndex = 15;
+            this.btnInventoryDelete.Text = "Delete";
+            this.btnInventoryDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnInventoryAdd
+            // 
+            this.btnInventoryAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventoryAdd.Location = new System.Drawing.Point(24, 188);
+            this.btnInventoryAdd.Name = "btnInventoryAdd";
+            this.btnInventoryAdd.Size = new System.Drawing.Size(114, 44);
+            this.btnInventoryAdd.TabIndex = 14;
+            this.btnInventoryAdd.Text = "Add";
+            this.btnInventoryAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnInventorySearch
+            // 
+            this.btnInventorySearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventorySearch.Location = new System.Drawing.Point(24, 108);
+            this.btnInventorySearch.Name = "btnInventorySearch";
+            this.btnInventorySearch.Size = new System.Drawing.Size(114, 44);
+            this.btnInventorySearch.TabIndex = 13;
+            this.btnInventorySearch.Text = "Search";
+            this.btnInventorySearch.UseVisualStyleBackColor = true;
+            // 
+            // lbxInventoryMenu
+            // 
+            this.lbxInventoryMenu.FormattingEnabled = true;
+            this.lbxInventoryMenu.ItemHeight = 16;
+            this.lbxInventoryMenu.Location = new System.Drawing.Point(298, 13);
+            this.lbxInventoryMenu.Name = "lbxInventoryMenu";
+            this.lbxInventoryMenu.Size = new System.Drawing.Size(301, 356);
+            this.lbxInventoryMenu.TabIndex = 12;
+            // 
+            // lblInventoryTitle
+            // 
+            this.lblInventoryTitle.AutoSize = true;
+            this.lblInventoryTitle.Font = new System.Drawing.Font("Times New Roman", 25.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInventoryTitle.Location = new System.Drawing.Point(15, 13);
+            this.lblInventoryTitle.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
+            this.lblInventoryTitle.Name = "lblInventoryTitle";
+            this.lblInventoryTitle.Size = new System.Drawing.Size(187, 51);
+            this.lblInventoryTitle.TabIndex = 11;
+            this.lblInventoryTitle.Text = "Inventory";
+            // 
+            // InventoryMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(614, 383);
+            this.Controls.Add(this.btnInventoryDelete);
+            this.Controls.Add(this.btnInventoryAdd);
+            this.Controls.Add(this.btnInventorySearch);
+            this.Controls.Add(this.lbxInventoryMenu);
+            this.Controls.Add(this.lblInventoryTitle);
+            this.Name = "InventoryMain";
+            this.Text = "InventoryMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInventoryDelete;
+        private System.Windows.Forms.Button btnInventoryAdd;
+        private System.Windows.Forms.Button btnInventorySearch;
+        private System.Windows.Forms.ListBox lbxInventoryMenu;
+        private System.Windows.Forms.Label lblInventoryTitle;
+    }
+}
